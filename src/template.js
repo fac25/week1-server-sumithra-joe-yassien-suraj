796a588420274c44951734df13fbf8614b49b59b
Sumithra-Suresh <jasisumi@gmail.com>
2022-09-22 14:03:56 +0100

    Added styles

--- src/template.js
+++ src/template.js
@@ -1,15 +1,19 @@
 function content(posts, errors = {}) {
   const postList = posts.map((post) => {
-    return `<div>
+    return `<div class="plate">
+    <span class="inner_plate">
+    <div>
         <h2>${sanitisation(post.name)}</h2>
         <p>${sanitisation(post.comments)}</p>
         <p>${sanitisation(post.date)}</p>
-        </div>`;
+        </div>
+        </span>
+</div>`;
   });
 
   const content = `
   <!DOCTYPE html>
-<html lang="en">
+  <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -20,35 +24,39 @@ function content(posts, errors = {}) {
     <meta name="description" content="" />
     <meta name="keywords" content="" />
     <meta name="author" content="Yassien, Joe, Suraj, Sumithra" />
+    <link rel="stylesheet" type="text/css" href="style.css"/>
   </head>
   <body>
     <header>
-      <h1>Food Opinions!</h1>
+    <h1 class="center width-sm"> Food Opinions! </h1>
     </header>
 
     <main>
       <!-- Submit new comments goes here -->
       <section>
-        <form method="POST">
-          <label for="username">Username</label>
+      <form method="POST" class="center width-sm">
+      <label class="sr-only" for="username">Username</label>
           <input id="username" name="username" placeholder="Name" />
           ${validation(errors.name)}
           <br />
           <br />
           <label for="opinion"></label>
-          <textarea id="opinion" name="opinion"></textarea>
+          <textarea id="opinion" name="opinion"></textarea><br><br>
           ${validation(errors.comments)}
 
-          <button type="submit">Post</button>
+          <button type="submit">Serve up your opinion!</button>
         </form>
       </section>
 
       <!-- User comments go here -->
-      <section>
-        <ul>
-          ${postList.join("")}
+        <div class="table-background">
+          <section class="center width-lg">
+          <ul class="grid">
+              ${postList.join("")}
+          
         </ul>
       </section>
+      </div>
     </main>
   </body>
 </html> `;
